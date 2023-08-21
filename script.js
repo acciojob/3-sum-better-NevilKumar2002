5180d219fa12e07a0ebb02dcbd24e5d355fd2614
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-21 08:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-let arr=[-1,2,1,-4];
-let target=1;
-function threeSum(arr, target) {
+
+
 // write your code here
-	let ans=1;
-	for(let i=0 ;i<arr.length; i++)
-		{
-			for(let j=0 ;j<arr.length ;j++)
-				{
-					for(let k=0;k<arr.length ;k++)
-						{
-							let a= arr[i]+arr[j]+arr[k] ;
-							if(ans<a)
-							{
-								ans=a;
-							}
-						}
-				}
-		}
-	console.log(ans); 
-  
+	function threeSum(arr, target) {
+    // Sort the array
+    arr.sort((a, b) => a - b);
+    
+    let closestSum = Infinity;
+    
+    // For each number in the array
+    for (let i = 0; i < arr.length - 2; i++) {
+        // Use two pointers to find a pair that makes the sum of the three numbers closest to the target
+        let start = i + 1;
+        let end = arr.length - 1;
+        
+        while (start < end) {
+            let sum = arr[i] + arr[start] + arr[end];
+            
+            // If the sum is closer to the target than the current closest sum, update the closest sum
+            if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                closestSum = sum;
+            }
+            
+            // If the sum is less than the target, move the start pointer up
+            if (sum < target) {
+                start++;
+            // If the sum is more than the target, move the end pointer down
+            } else {
+                end--;
+            }
+        }
+    }
+    
+    return closestSum;
 }
-threeSum(arr,target);
+	
+  
+
+// threeSum(arr,target);
 
 module.exports = threeSum;
