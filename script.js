77c0545db18e1dfa9b81a920a382215ff911909f
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-21 08:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function threeSum(arr, target) {
 // write your code here
+	let ans=1;
+	for(let i=0 ;i<arr.length; i++)
+		{
+			for(let j=0 ;j<arr.length ;j++)
+				{
+					for(let k=0;k<arr.length ;k++)
+						{
+							let a= arr[i]+arr[j]+arr[k] ;
+							if(ans<a)
+							{
+								ans=a;
+							}
+						}
+				}
+		}
+	return ans;
   
 }
 
